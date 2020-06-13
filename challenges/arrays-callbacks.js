ca457f0838d942fac1666ecbf47776d46c48281b
Sharon-Mosher <64615121+Sharon-Mosher@users.noreply.github.com>
2020-06-12 23:59:00 -0400

    closure finished. working on prototypes

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -94,10 +94,10 @@ function multiply(num1,num2){
 function greeting(firstName,lastName){
 	return `Hello ${firstName} ${lastName} , nice to meet you!`;}
 
-// /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// // console.log(consume(2, 2, add)); // 4
-// // console.log(consume(10, 16, multiply)); // 160
-// // console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+/* Step 3: Check your work by un-commenting the following calls to consume(): */
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
